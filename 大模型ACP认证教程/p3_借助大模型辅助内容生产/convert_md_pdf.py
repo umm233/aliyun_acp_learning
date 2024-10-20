144572775dc5ec1f8ba3d264da99a599f9d38126
haoznic <haoznic@gmail.com>
2024-10-20 17:08:22 +0800

    bugfix 3.3

--- 大模型ACP认证教程/p3_借助大模型辅助内容生产/convert_md_pdf.py
+++ 大模型ACP认证教程/p3_借助大模型辅助内容生产/convert_md_pdf.py
@@ -3,9 +3,7 @@ from PIL import Image
 import subprocess
 from pdf2image import convert_from_path
 import argparse
-import os
-
-
+import time
 
 def convert_single_md_to_pdf_and_png(md_file_path, output_base_dir="./Marp/"):
     """
@@ -48,7 +46,62 @@ def convert_single_md_to_pdf_and_png(md_file_path, output_base_dir="./Marp/"):
         print(f"成功将 '{pdf_file}' 转换为PNG图像并保存至 '{output_dir}'。")
     except subprocess.CalledProcessError as e:
         print(f"转换 '{md_file_path}' 时发生错误: {e}")
+        print("\n尝试另外一种生成方法，先转换为pptx再生成pdf:")
+        convert_single_md_to_pptx_pdf_and_png(md_file_path, output_base_dir)        
 
+
+def convert_single_md_to_pptx_pdf_and_png(md_file_path, output_base_dir="./Marp/"):
+    """
+    将单个MD文件转换为PDF和PNG格式，PDF和PNG文件将保存在由MD文件名决定的子目录中。
+
+    :param md_file_path: MD文件的完整路径。
+    :param output_base_dir: 输出目录的基路径，将在此目录下创建子目录以保存输出文件。
+    """
+
+    if not os.path.exists(output_base_dir):
+        os.makedirs(output_base_dir)
+
+    # 获取MD文件名（去掉.md后缀）
+    filename = os.path.splitext(os.path.basename(md_file_path))[0]
+
+    # 创建输出目录，目录名与MD文件名相同
+    output_dir = os.path.join(output_base_dir, filename)
+    os.makedirs(output_dir, exist_ok=True)  # 如果目录已存在，则不会抛出异常
+    
+    command = ["marp", "--html", "--allow-local-files",
+               "--output", os.path.join(output_dir, filename + ".pptx"),
+               md_file_path]
+
+    try:
+        # 执行命令，将MD文件转换为PDF
+        # print(command)
+        subprocess.run(command, check=True)
+        print(f"成功将 '{md_file_path}' 转换为pptx并保存至 '{output_dir}'。")
+        
+        # 将pptx转换为pdf图像
+        pptx_file = os.path.join(output_dir, filename + ".pptx")        
+        command = ["libreoffice", "--headless", "--convert-to","pdf:writer_pdf_Export",
+               "--outdir", os.path.join(output_dir),
+               pptx_file]
+
+        # print(command)
+        subprocess.run(command, check=True)
+        print(f"成功将 '{md_file_path}' 转换为PDF并保存至 '{output_dir}'。")
+        
+        time.sleep(5)
+        
+        # 将PDF转换为PNG图像
+        pdf_file = os.path.join(output_dir, filename + ".pdf")
+        print("pdf_file:",pdf_file)
+        pages = convert_from_path(pdf_file, dpi=300)  # dpi参数可以根据需要调整
+
+        # 遍历每一页并保存为PNG格式
+        for i, page in enumerate(pages):
+            file_name = os.path.join(output_dir, f"{filename}_{i+1}.png")
+            page.save(file_name, 'PNG')
+        print(f"成功将 '{pdf_file}' 转换为PNG图像并保存至 '{output_dir}'。")
+    except subprocess.CalledProcessError as e:
+        print(f"转换 '{md_file_path}' 时发生错误: {e}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="处理文件并输出到指定目录。")
